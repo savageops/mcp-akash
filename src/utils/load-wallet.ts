d8e4e5be3cb90ca89c4de4cef1e6c7417b9cd689
Savage <savagejayog@gmail.com>
2025-09-26 20:52:03 +0200

    Mnemonic takes priority

--- src/utils/load-wallet.ts
+++ src/utils/load-wallet.ts
@@ -6,13 +6,13 @@ import { SERVER_CONFIG } from '../config.js';
 export async function loadWalletAndClient() {
   let wallet: DirectSecp256k1HdWallet | DirectSecp256k1Wallet;
   
-  if (SERVER_CONFIG.privateKey) {
-    const privateKeyBytes = Uint8Array.from(Buffer.from(SERVER_CONFIG.privateKey, 'hex'));
-    wallet = await DirectSecp256k1Wallet.fromKey(privateKeyBytes, 'akash');
-  } else if (SERVER_CONFIG.mnemonic) {
+  if (SERVER_CONFIG.mnemonic) {
     wallet = await DirectSecp256k1HdWallet.fromMnemonic(SERVER_CONFIG.mnemonic, {
       prefix: 'akash',
     });
+  } else if (SERVER_CONFIG.privateKey) {
+    const privateKeyBytes = Uint8Array.from(Buffer.from(SERVER_CONFIG.privateKey, 'hex'));
+    wallet = await DirectSecp256k1Wallet.fromKey(privateKeyBytes, 'akash');
   } else {
     throw new Error('Either AKASH_MNEMONIC or AKASH_PRIVATE_KEY must be provided');
   }
